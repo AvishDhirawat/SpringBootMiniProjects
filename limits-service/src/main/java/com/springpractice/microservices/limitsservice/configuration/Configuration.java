0cc6c88eeee42f2a492c29207c22a2c77df0f3d0
avishdhirawat <avishdhirawat0@gmail.com>
2024-03-14 01:16:57 +0530

    Limits-Service App : Created Configuration.java so that it will pick values defined in application.properties

---
+++ limits-service/src/main/java/com/springpractice/microservices/limitsservice/configuration/Configuration.java
@@ -0,0 +1,25 @@
+package com.springpractice.microservices.limitsservice.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("limit-service") // This will take configuration named "limit-service" from application.properties
+public class Configuration {
+    private int minimum;
+    private int maximum;
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(int minimum) {
+        this.minimum = minimum;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+}
--- limits-service/src/main/java/com/springpractice/microservices/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/springpractice/microservices/limitsservice/controller/LimitsController.java
@@ -1,15 +1,21 @@
 package com.springpractice.microservices.limitsservice.controller;
 
 import com.springpractice.microservices.limitsservice.bean.Limits;
+import com.springpractice.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration; // Using Autowired so that it will pick values from Configuration class
+
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-        return  new Limits(1,1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+//        return  new Limits(1,1000);
     }
 
 }

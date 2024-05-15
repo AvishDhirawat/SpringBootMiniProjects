07404817b340f69ba9ae2217ae575ea8b5525141
avishdhirawat <avishdhirawat0@gmail.com>
2024-05-15 20:15:44 +0530

    CurrencyExchange Service: Added environment port number.

--- currency-exchange-service/src/main/java/com/springpractice/microservices/currencyexchangeservice/currencyexchange/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/springpractice/microservices/currencyexchangeservice/currencyexchange/CurrencyExchange.java
@@ -7,12 +7,13 @@ public class CurrencyExchange {
     private String from;
     private String to;
     private BigDecimal conversionMultiple;
+    private String environment;
 
     public CurrencyExchange() {
 
     }
 
-    public CurrencyExchange(Long id, String from,  String to, BigDecimal conversionMultiple) {
+    public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
         this.id = id;
         this.from = from;
         this.conversionMultiple = conversionMultiple;
@@ -49,5 +50,13 @@ public class CurrencyExchange {
 
     public void setConversionMultiple(BigDecimal conversionMultiple) {
         this.conversionMultiple = conversionMultiple;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
     }
 }

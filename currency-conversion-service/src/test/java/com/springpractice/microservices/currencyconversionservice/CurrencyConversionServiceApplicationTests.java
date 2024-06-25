66e7c3f9ecbeec848e6e820a9e8cb6dc0c3190f1
Avish Dhirawat <31956212+AvishDhirawat@users.noreply.github.com>
2024-06-25 21:01:29 +0530

    Add files via upload

---
+++ currency-conversion-service/src/test/java/com/springpractice/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpractice.microservices.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

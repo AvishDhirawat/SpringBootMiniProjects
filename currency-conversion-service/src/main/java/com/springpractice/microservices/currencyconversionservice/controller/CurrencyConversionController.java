e8e037778a291c5ac9fc2d601e94e811c876607b
avishdhirawat <avishdhirawat0@gmail.com>
2024-08-09 21:22:21 +0530

    Currency Conversion Service : Working

--- currency-conversion-service/src/main/java/com/springpractice/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/springpractice/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -2,11 +2,14 @@ package com.springpractice.microservices.currencyconversionservice.controller;
 
 import com.springpractice.microservices.currencyconversionservice.CurrencyConversion;
 import com.springpractice.microservices.currencyconversionservice.CurrencyConversionServiceApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
@@ -17,7 +20,16 @@ public class CurrencyConversionController {
             @PathVariable BigDecimal quantity
     ) {
 
-        return new CurrencyConversion(1001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate()
+                .getForEntity("https://localhost:8080/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+
+        return new CurrencyConversion(currencyConversion.getId(), from, to, quantity, currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment());
 
     }
 }
